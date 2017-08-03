7b93a0e1568f59bf9b70996f36c0e3133cb86ae2
JSNU-Bighead <1162025143@qq.com>
2017-08-03 16:54:48 +0800

    doc

--- src/views/Viewer.vue
+++ src/views/Viewer.vue
@@ -11,23 +11,17 @@
                     {
                         fileName: "柯基犬.jpeg",
                         filePath:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1501388665141&di=39690311e5f9a0c4cbe6f69a8bfa57af&imgtype=0&src=http%3A%2F%2Fimg4.duitang.com%2Fuploads%2Fitem%2F201510%2F27%2F20151027185454_wVz3i.jpeg",
-                        fileType:"JPEG",
-                        isImage:true,
-                        isPDF:false
+                        fileType:"JPEG"
                     },
                     {
                         fileName:"download_SalaryCertificate201703241123414010.pdf",
                         filePath:"https://code.visualstudio.com/shortcuts/keyboard-shortcuts-windows.pdf",
-                        fileType:"PDF",
-                        isImage:false,
-                        isPDF:true
+                        fileType:"PDF"
                     },
                     {
                         fileName: "柯基犬.jpg",
                         filePath:"https://gss3.bdstatic.com/7Po3dSag_xI4khGkpoWK1HF6hhy/baike/w%3D268%3Bg%3D0/sign=f685709bf303738dde4a0b248b20d76c/77c6a7efce1b9d16b27e955af6deb48f8c54644b.jpg",
-                        fileType:"JPG",
-                        isImage:true,
-                        isPDF:false
+                        fileType:"JPG"
                     },
                 ]
             }
