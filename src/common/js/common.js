967775b21431a584c6175601a54fbf36f57159f9
JSNU-Bighead <1162025143@qq.com>
2018-01-02 16:03:47 +0800

    Update common.js
    指令化文件查看组件

--- src/common/js/common.js
+++ src/common/js/common.js
@@ -4,3 +4,41 @@ import viewer from '../components/viewer/viewer.vue'
 // 组件注册
 Vue.component('v-select',select)
 Vue.component('v-viewer',viewer)
+//指令
+Vue.directive('filePreview', {
+    bind() { },
+    inserted(el, binding, vnode) {
+        var Vue = vnode.context.$root.constructor;
+        var Preview = Vue.extend(viewer);
+        var hook = document.createElement('div');
+        document.body.appendChild(hook);
+        var instance = new Preview({
+            el: hook,
+            propsData: {
+                viewerList: [
+                    {
+                        filePath: binding.value.dataUrl,
+                        fileName: binding.value.name,
+                    }
+                ]
+            }
+        });
+        el.addEventListener('click', () => {
+            instance.showFn();
+        })
+        el.instance = instance;
+    },
+    update(el, binding, vnode) {
+        el.instance.viewerList = [
+            {
+                filePath: binding.value.dataUrl,
+                fileName: binding.value.name,
+                marginTop: 0
+            }
+        ];
+    },
+    unbind(el, binding, vnode) {
+        el.instance.$el.remove();
+        el.instance.$destroy();
+    }
+})
